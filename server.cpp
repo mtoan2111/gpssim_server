bb3a6047a117c0309676522f842b4ae11530610e
Nguyễn Mạnh Toàn <toan93.hust@gmail.com>
2018-05-16 11:24:47 +0700

    first init

---
+++ server.cpp
@@ -0,0 +1,450 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <conio.h>
+#include <WinSock2.h>
+
+int USER_MOTION = 3000;
+#define MAX_LEN 100
+#define WGS84_RADIUS 6378137.0
+#define WGS84_ECCENTRICITY 0.0818191908426
+#define V_MAX 27.7
+#define R2D 57.2957795131
+
+void subVect(double *A1, double *A2, double *result)
+{
+  result[0] = A1[0] - A2[0];
+  result[1] = A1[1] - A2[1];
+  result[2] = A1[2] - A2[2];
+}
+
+bool isSimillar(double *A, double *B)
+{
+  if (A[0] == B[0])
+    return 0;
+  if (A[1] == B[1])
+    return 0;
+  if (A[2] == B[2])
+    return 0;
+  return 1;
+}
+
+double normVect(double *A)
+{
+  return sqrt(pow(A[0], 2) + pow(A[1], 2) + pow(A[2], 2));
+}
+
+double detectDirection(double *A, double *B, double *C)
+{
+  if (!(isSimillar(A, B) && isSimillar(B, C)))
+    return 0;
+  double AB[3];
+  subVect(B, A, AB);
+  double cABC, cosABC, norm;
+  cABC = AB[0] * C[0] + AB[1] * C[1] + AB[2] * C[2];
+  norm = (normVect(AB)*normVect(C));
+  cosABC = cABC / norm;
+  if (cosABC > 1)
+  {
+    cosABC = 1;
+  }
+  if (cosABC < -1)
+  {
+    cosABC = -1;
+  }
+//{
+//	printf("%lf \n", acos(cosABC) * R2D);
+//}
+  return (acos(cosABC)*R2D);
+}
+
+double calculateAngle(double *A, double *B, double *C)
+{
+  if (!(isSimillar(A, B) && isSimillar(B, C)))
+    return 0;
+  double AB[3], BC[3];
+  subVect(B, A, AB);
+  subVect(B, C, BC);
+  double cABC, cosABC, norm;
+  cABC = AB[0] * BC[0] + AB[1] * BC[1] + AB[2] * BC[2];
+  norm = (normVect(AB)*normVect(BC));
+  cosABC = cABC / norm;
+  if (cosABC > 1)
+  {
+    cosABC = 1;
+  }
+  if (cosABC < -1)
+  {
+    cosABC = -1;
+  }
+  return acos(cosABC)*R2D;
+}
+
+void readUserMotion(double **xyz, FILE *fp)
+{
+  int num;
+  char str[MAX_LEN];
+  double t, x, y, z;
+  for (num = 0; num < USER_MOTION; num++)
+  {
+    if (fgets(str, MAX_LEN, fp) == NULL)
+      break;
+
+    if (EOF == sscanf(str, "%lf,%lf,%lf,%lf", &t, &x, &y, &z))
+      break;
+    xyz[num][0] = x;
+    xyz[num][1] = y;
+    xyz[num][2] = z;
+  }
+  USER_MOTION = num;
+  return;
+}
+
+double normVector(double *xyz)
+{
+  return sqrt(pow(xyz[0], 2) + pow(xyz[1], 2) + pow(xyz[2], 2));
+}
+
+void xyz2llh(double **xyz, double **llh)
+{
+  int num;
+  double a, eps, e, e2;
+  double x, y, z;
+  double rho2, dz, zdz, nh, slat, n, dz_new;
+    
+  a = WGS84_RADIUS;
+  e = WGS84_ECCENTRICITY;
+
+  eps = 1.0e-3;
+  e2 = pow(e, 2);
+  for (num = 0; num < USER_MOTION; num++)
+  {
+    if (normVector(xyz[num]) < eps)
+    {
+      llh[num][0] = 0.0;
+      llh[num][1] = 0.0;
+      llh[num][2] = -a;
+      continue;
+    }
+    x = xyz[num][0];
+    y = xyz[num][1];
+    z = xyz[num][2];
+
+    rho2 = pow(x, 2) + pow(y, 2);
+    dz = e2 * z;
+    while (true)
+    {
+      zdz = z + dz;
+      nh = sqrt(rho2 + pow(zdz, 2));
+      slat = zdz / nh;
+      n = a / sqrt(1.0 - e2*pow(slat, 2));
+      dz_new = n*e2*slat;
+
+      if (fabs(dz - dz_new) < eps)
+      {
+        break;
+      }
+      dz = dz_new;
+    }
+    llh[num][0] = atan2(zdz, sqrt(rho2))*R2D;
+    llh[num][1] = atan2(y, x)*R2D;
+    llh[num][2] = (nh - n)*R2D;
+  }
+  return;
+}
+
+void tmat(double *llh, double t[3][3])
+{
+  double slat, clat;
+  double slon, clon;
+
+  slat = sin(llh[0]);
+  clat = cos(llh[0]);
+  slon = sin(llh[1]);
+  clon = cos(llh[1]);
+
+  t[0][0] = -slat * clon;
+  t[0][1] = -slat * slon;
+  t[0][2] = clat;
+  t[1][0] = -slon;
+  t[1][1] = clon;
+  t[1][2] = 0.0;
+  t[2][0] = clat * clon;
+  t[2][1] = clat * slon;
+  t[2][2] = slat;
+}
+
+double calculateSpeed(double *neu1, double *neu2)
+{
+  double distance, t;
+  t = 0.1;
+
+  distance = sqrt(pow((neu2[0] - neu1[0]), 2) + pow((neu2[1] - neu1[1]), 2));
+  return distance / t;
+}
+
+void xyz2neu(double **xyz, double **llh, double **neu)
+{
+  int num;
+  double t[3][3];
+  for (num = 0; num < USER_MOTION; num++)
+  {
+    tmat(llh[num], t);
+    neu[num][0] = t[0][0] * xyz[num][0] + t[0][1] * xyz[num][1] + t[0][2] * xyz[num][2];
+    neu[num][1] = t[1][0] * xyz[num][0] + t[1][1] * xyz[num][1] + t[1][2] * xyz[num][2];
+    neu[num][2] = t[2][0] * xyz[num][0] + t[2][1] * xyz[num][1] + t[2][2] * xyz[num][2];
+  }
+}
+
+void rmReduantNeu(double **neu, double **neu_new)
+{
+  int num;
+  for (num = 0; num < USER_MOTION / 2; num++)
+  {
+    neu_new[num][0] = neu[num][0];
+    neu_new[num][1] = neu[num][1];
+    neu_new[num][2] = neu[num][2];
+  }
+}
+
+void calWrongNeu( double **xyz_wrong, double **xyz, double **llh)
+{
+  int num;
+  double velocity, tm;
+  double t[3][3];
+  double neu[3];
+  velocity = V_MAX;
+  tm = 0.1;
+  tmat(llh[0], t);
+  for (num = 0; num < USER_MOTION; num++)
+  {
+    neu[0] = 0;
+    neu[1] = 0;
+    neu[2] = 0;
+    if (num > 2)
+    {
+      double angle = calculateAngle(xyz[num - 2], xyz[num - 1], xyz[num]);
+      if (angle < 170)
+      {
+        //drones redirected
+        double subN = xyz[num][0] - xyz[num - 1][0];
+        double subE = xyz[num][1] - xyz[num - 1][1];
+        /*		N
+        IV  |  I
+        --------- E
+        III |  II
+        */
+        // I
+        if (subN > 0 && subE > 0)
+        {
+          neu[0] = (velocity * tm)*cos((180 - angle)*R2D) *2;
+          neu[1] = (velocity * tm)*sin((180 - angle)*R2D) *2;
+        }
+        // II
+        if (subN < 0 && subE > 0)
+        {
+          neu[0] = -(velocity * tm)*cos((180 - angle)*R2D) *2;
+          neu[1] = (velocity * tm)*sin((180 - angle)*R2D) *2;
+        }
+        // III
+        if (subN < 0 && subE < 0)
+        {
+          neu[0] = -(velocity * tm)*cos((180 - angle)*R2D) *2;
+          neu[1] = -(velocity * tm)*sin((180 - angle)*R2D) *2;
+        }
+        // IV
+        if (subN > 0 && subE < 0)
+        {
+          neu[0] = (velocity * tm)*cos((180 - angle)*R2D) *2;
+          neu[1] = -(velocity * tm)*sin((180 - angle)*R2D) *2;
+        }
+      }
+      else
+      {
+        double xVect[3];
+        xVect[0] = 0.0;
+        xVect[1] = 1.0;
+        xVect[2] = 0.0;
+        double angle = detectDirection(xyz[num - 2], xyz[num - 1], xVect);
+        if (angle < 45 || angle > 135)
+        {
+          neu[1] = -(velocity * tm);
+        }
+        else
+        {
+          neu[0] = -(velocity * tm);
+        }
+      }
+    }
+    xyz_wrong[num][0] = xyz[num][0] + t[0][0] * neu[0] + t[0][1] * neu[1] + t[0][2] * neu[2];
+    xyz_wrong[num][1] = xyz[num][1] + t[1][0] * neu[0] + t[1][1] * neu[1] + t[1][2] * neu[2];
+    xyz_wrong[num][2] = xyz[num][2] + t[2][0] * neu[0] + t[2][1] * neu[1] + t[2][2] * neu[2];
+  }
+}
+
+double **createBuff(int i, int j)
+{
+  double **array = (double **)malloc(i * sizeof(double **));
+  for (int m = 0; m < i; m++)
+  {
+    array[m] = (double *)malloc(j * sizeof(double));
+  }
+  return array;
+}
+
+int main(int argc, char *argv)
+{
+  int ret;
+  WSADATA data;
+  ret = WSAStartup(MAKEWORD(2, 2), &data);
+  if (ret == -1)
+  {
+    printf("Error 1!");
+    return 0;
+  }
+  SOCKET s, c;
+  sockaddr_in Saddr, Caddr;
+  Saddr.sin_family = AF_INET;
+  Saddr.sin_port = htons(8080);
+  Saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  //--> Create socket
+  s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  //--> Binding socket
+  ret = bind(s, (sockaddr*)&Saddr, sizeof(Saddr));
+  if (ret == -1)
+  {
+    printf("Error 2!");
+    return 0;
+  }
+  //--> Open queue
+  listen(s, 10);
+  FILE *in;
+  FILE *out;
+  FILE *out_llh;
+  FILE *out_neu;
+  FILE *out_llh_wrong;
+  double **xyz;
+  double **xyz_wrong;
+  double **llh;
+  double **neu;
+  double **neu_new;
+  double **llh_wrong;
+  float tp;
+  char *input_source = "D:/GitReposities/gps-sdr-sim/simulator.csv";
+  char *output_source = "D:/GitReposities/gps-sdr-sim/spoof.csv";
+  char *output_llh_source = "D:/GitReposities/gps-sdr-sim/out_llh.csv";
+  char *output_neu_source = "D:/GitReposities/gps-sdr-sim/out_neu.csv";
+  char *output_llh_wrong = "D:/GitReposities/gps-sdr-sim/spoof_llh.csv";
+  in = fopen(input_source, "r");
+  out = fopen(output_source, "wb");
+  out_llh = fopen(output_llh_source, "wb");
+  out_neu = fopen(output_neu_source, "wb");
+  out_llh_wrong = fopen(output_llh_wrong, "wb");
+  if (in == NULL)
+  {
+    printf("Can't open motion file. Exit!\n");
+    return 0;
+  }
+  //-->Read all line of user motion file and store into xyz
+  xyz = createBuff(USER_MOTION, 3);
+  readUserMotion(xyz, in);
+  //-->Convert all xyz coordinates to llh coordinates
+  llh = createBuff(USER_MOTION, 3);
+  xyz2llh(xyz, llh);
+  tp = 0.0;
+  for (int i = 0; i < USER_MOTION; i++)
+  {
+    char buff1[100];
+    sprintf(buff1, "  %.1f, %lf, %lf, %lf\n", tp, llh[i][0], llh[i][1], llh[i][2]);
+    fwrite(buff1, strlen(buff1), 1, out_llh);
+    tp = tp + (0.1 * 10) / 10.0;
+  }
+  //-->Convert all xyz coordinates to neu coordinates
+  neu = createBuff(USER_MOTION, 3);
+  xyz2neu(xyz, llh, neu);
+  tp = 0.0;
+  for (int i = 0; i < USER_MOTION; i++)
+  {
+    char buff1[100];
+    sprintf(buff1, "  %.1f, %lf, %lf, %lf\n", tp, neu[i][0], neu[i][1], neu[i][2]);
+    fwrite(buff1, strlen(buff1), 1, out_neu);
+    tp = tp + (0.1 * 10) / 10.0;
+  }
+  //-->Remove some reduant coordinates
+  //neu_new = createBuff(USER_MOTION, 3);
+  //rmReduantNeu(neu, neu_new);
+  //-->Calculate wrong coordinates from neu coordinates
+  xyz_wrong =createBuff(USER_MOTION, 3);
+  calWrongNeu(xyz_wrong, xyz, llh);
+  tp = 0.0;
+  for (int i = 0; i < USER_MOTION; i++)
+  {
+    char buff[100];
+    sprintf(buff, "  %.1f,%.3lf, %.3lf, %.3lf\n", tp, xyz_wrong[i][0], xyz_wrong[i][1], xyz_wrong[i][2]);
+    fwrite(buff, strlen(buff), 1, out);
+    tp = tp + (0.1*10)/10.0;    
+  }
+  llh_wrong = createBuff(USER_MOTION, 3);
+  xyz2llh(xyz_wrong, llh_wrong);
+  tp = 0.0;
+  for (int i = 0; i < USER_MOTION; i++)
+  {
+    char buff1[100];
+    sprintf(buff1, "  %.1f, %lf, %lf, %lf\n", tp, llh_wrong[i][0], llh_wrong[i][1], llh_wrong[i][2]);
+    fwrite(buff1, strlen(buff1), 1, out_llh_wrong);
+    tp = tp + (0.1 * 10) / 10.0;
+  }
+  //waiting client connect
+  int len = sizeof(Caddr);
+  printf("Waiting client connect\n");
+  c = accept(s, (sockaddr *)&Caddr, &len);
+  //-->send first xyz to calculate tmat, neu
+  printf("Success!\n");
+  char fist_mess[100];
+  char ok_mess[8];
+  sprintf(fist_mess, "%.3lf, %.3lf, %.3lf\n", llh_wrong[0][0], llh_wrong[0][1], llh_wrong[0][2]);
+  ret = recv(c, ok_mess, sizeof(ok_mess), 0);
+  if (ret == -1)
+  {
+    printf("Error 3!\n");
+    return 0;
+  }
+  else
+  {
+    if (strcmp(ok_mess, "ok") == 0)
+    {
+      send(c, fist_mess, sizeof(fist_mess), 0);
+    }
+  }
+  //-->send data
+  for (int i = 0; i < USER_MOTION; i++)
+  {
+    char buff[100];
+    char ok[8];
+    sprintf(buff, "%.3lf, %.3lf, %.3lf\n", xyz_wrong[i][0], xyz_wrong[i][1], xyz_wrong[i][2]);
+    ret = recv(c, ok, sizeof(ok), 0);
+    if (ret == -1)
+    {
+      printf("Error 3!\n");
+      return 0;
+    }
+    else
+    {
+      if (strncmp(ok, "OK",2) == 0)
+      {
+        printf("%s\n", buff);
+        send(c, buff, sizeof(buff), 0);
+      }
+    }
+  }
+  closesocket(c);
+  fclose(in);
+  fclose(out);
+  fclose(out_llh);
+  fclose(out_neu);
+  fclose(out_llh_wrong);
+  //-->save coordinates
+  _getch();
+  return 0;
+}
